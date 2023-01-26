c97836d787df30107958df3030eeb78ec5839788
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-26 16:22:59 +0530

    edit main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import pygame
 pygame.init()
 
 # Game Constants:
-HEIGHT = 800
-WIDTH = 600
+HEIGHT = 600
+WIDTH = 800
 BLACK = (0, 0, 0)
 
 # Setting up the screen
-screen = pygame.display.set_mode((HEIGHT, WIDTH))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Screen title
 pygame.display.set_caption("Space Invaders")
@@ -18,10 +18,14 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load()
+
 
 
 # UFO Icon Credit "<a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by Pixel
 # Buddha - Flaticon</a>"
+# Spaceship icon "<a href="https://www.flaticon.com/free-icons/spaceship" title="spaceship icons">Spaceship icons created by Freepik - Flaticon</a>"
 # Game Loop
 running = True
 while running:
