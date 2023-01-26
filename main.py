0de413606f79fbd25a1f1f8cf6aed0891036420c
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-26 17:11:59 +0530

    player movement functional

--- main.py
+++ main.py
@@ -19,8 +19,14 @@ icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load()
+playerImg = pygame.image.load("player.png")
+playerX = 370
+playerY = 480
+playerX_change = 0
 
+
+def player(x, y):
+    screen.blit(playerImg, (playerX, playerY))
 
 
 # UFO Icon Credit "<a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by Pixel
@@ -29,9 +35,21 @@ playerImg = pygame.image.load()
 # Game Loop
 running = True
 while running:
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(BLACK)
+        # Player movements:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change -= 0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change += 0.3
+
+        if event.type == pygame.KEYUP:
+            playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
     pygame.display.update()
