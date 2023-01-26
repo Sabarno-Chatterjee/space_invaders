d48a80b1fa034c177f1bda3f0d638af453aef582
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-26 20:08:59 +0530

    added bullet, need to add functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize pygame
 pygame.init()
@@ -26,8 +27,19 @@ playerX_change = 0
 
 # Enemy
 enemyImg = pygame.image.load("enemy.png")
-enemyX = 370
-enemyY = 50
+enemyX = random.randint(0, 736)
+enemyY = random.randint(50, 150)
+enemyX_change = 4
+enemyY_change = 40
+
+# Bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = 480
+bulletY_change = 10
+bullet_state = "ready"
+# Background
+backgroundIMG = pygame.image.load("background.png")
 
 
 def player(x, y):
@@ -38,15 +50,24 @@ def enemy(x, y):
     screen.blit(enemyImg, (x, y))
 
 
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
 # UFO Icon Credit "<a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by Pixel
 # Buddha - Flaticon</a>"
 # Spaceship icon "<a href="https://www.flaticon.com/free-icons/spaceship" title="spaceship icons">Spaceship icons created by Freepik - Flaticon</a>"
 # Game Loop
 # Invader <a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by smalllikeart - Flaticon</a>
+# <a href="https://www.flaticon.com/free-icons/bullet" title="bullet icons">Bullet icons created by Darius Dan - Flaticon</a>
+
 
 running = True
 while running:
     screen.fill(BLACK)
+    screen.blit(backgroundIMG, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -54,19 +75,38 @@ while running:
         # Player movements:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change -= 0.3
+                playerX_change -= 7
             if event.key == pygame.K_RIGHT:
-                playerX_change += 0.3
+                playerX_change += 7
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+                print("fire")
 
         if event.type == pygame.KEYUP:
             playerX_change = 0
 
     playerX += playerX_change
+    enemyX += enemyX_change
 
+    # Enemy movements:
+    if enemyX < 0:
+        enemyX_change = 2
+        enemyY += enemyY_change
+    elif enemyX > 736:
+        enemyX_change = - 2
+        enemyY += enemyY_change
+
+    # Bullet movement:
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
+    # Player movements:
     if playerX < 0:
         playerX = 0
     elif playerX > 736:
         playerX = 736
+
     player(playerX, playerY)
+
     enemy(enemyX, enemyY)
     pygame.display.update()
