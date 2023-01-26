edad1e46ad9dc06517dc61972c4aa5da318947a5
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-26 18:32:02 +0530

    spawns enemy

--- main.py
+++ main.py
@@ -24,15 +24,26 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
+# Enemy
+enemyImg = pygame.image.load("enemy.png")
+enemyX = 370
+enemyY = 50
+
 
 def player(x, y):
-    screen.blit(playerImg, (playerX, playerY))
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # UFO Icon Credit "<a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by Pixel
 # Buddha - Flaticon</a>"
 # Spaceship icon "<a href="https://www.flaticon.com/free-icons/spaceship" title="spaceship icons">Spaceship icons created by Freepik - Flaticon</a>"
 # Game Loop
+# Invader <a href="https://www.flaticon.com/free-icons/alien" title="alien icons">Alien icons created by smalllikeart - Flaticon</a>
+
 running = True
 while running:
     screen.fill(BLACK)
@@ -51,5 +62,11 @@ while running:
             playerX_change = 0
 
     playerX += playerX_change
+
+    if playerX < 0:
+        playerX = 0
+    elif playerX > 736:
+        playerX = 736
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
